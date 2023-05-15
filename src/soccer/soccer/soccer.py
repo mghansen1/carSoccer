6c3e398908ec6d7049f1a6a1ad19f2df352d4e71
Jinay Jain <jinaybjain@gmail.com>
2023-05-15 10:14:56 -0400

    apriltag, odometry, basic movement

--- src/soccer/soccer/soccer.py
+++ src/soccer/soccer/soccer.py
@@ -3,8 +3,10 @@ from rclpy.node import Node
 import rclpy
 from rclpy import qos
 
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Point, Quaternion
 from sensor_msgs.msg import Image
+from nav_msgs.msg import Odometry
+from apriltag_msgs.msg import AprilTagDetectionArray
 
 import cv2
 import numpy as np
@@ -23,8 +25,10 @@ from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 
 
+DO_MOVE = False
 TIMER_INTERVAL = 0.1
 TF_TIMER_INTERVAL = 0.5
+IMAGE_DIMS = np.array([320, 240])
 
 APRILTAG_POSITIONS = {
     "tag16h5:0": (-0.61, 2),
@@ -36,6 +40,43 @@ APRILTAG_POSITIONS = {
     "tag16h5:6": (-0.61, -2),
     "tag16h5:7": (-1.5, 0),
 }
+
+def prop(error, k, max_power, min_error:float=0):
+    out = np.clip(error * k, -max_power, max_power)
+
+    if abs(error) < min_error:
+        out = 0.0
+
+    return float(out)
+
+def to_np(p: Point):
+    return np.array([p.x, p.y, p.z])
+
+
+# Taken from: https://gist.github.com/salmagro/2e698ad4fbf9dae40244769c5ab74434
+def euler_from_quaternion(quaternion: Quaternion):
+    """
+    Converts quaternion (w in last place) to euler roll, pitch, yaw
+    quaternion = [x, y, z, w]
+    Bellow should be replaced when porting for ROS 2 Python tf_conversions is done.
+    """
+    x = quaternion.x
+    y = quaternion.y
+    z = quaternion.z
+    w = quaternion.w
+
+    sinr_cosp = 2 * (w * x + y * z)
+    cosr_cosp = 1 - 2 * (x * x + y * y)
+    roll = np.arctan2(sinr_cosp, cosr_cosp)
+
+    sinp = 2 * (w * y - z * x)
+    pitch = np.arcsin(sinp)
+
+    siny_cosp = 2 * (w * z + x * y)
+    cosy_cosp = 1 - 2 * (y * y + z * z)
+    yaw = np.arctan2(siny_cosp, cosy_cosp)
+
+    return np.array([roll, pitch, yaw])
 
 
 class MoveState(Enum):
@@ -61,6 +102,7 @@ class Soccer(Node):
         self.label_set = set()
 
         self.move_state = MoveState.FORWARD
+        self.turn = 0.0
 
         self.camera_subscription = self.create_subscription(
             Image,
@@ -70,43 +112,102 @@ class Soccer(Node):
         )
         self.camera_subscription
 
-        self.tf_buffer = Buffer()
-        self.tf_listener = TransformListener(self.tf_buffer, self)
+        self.odom_subscription = self.create_subscription(
+            Odometry,
+            '/zelda/odom',
+            self.odom_callback,
+            qos.qos_profile_sensor_data
+        )
+        self.odom_subscription
 
-        self.tf_timer = self.create_timer(TF_TIMER_INTERVAL, self.tf_timer_callback)
+        self.base_position = None
+        self.base_orientation = None
+        self.position = None
+        self.orientation = None
 
-    def tf_timer_callback(self):
-        try:
-            transform = self.tf_buffer.lookup_transform(
-                "camera_color_optical_frame", "tag16h5:4", rclpy.time.Time()
-            )
-            print(transform.transform.translation)
-        except TransformException as e:
-            print(e)
+        self.apriltag_subscription = self.create_subscription(
+            AprilTagDetectionArray,
+            "/detections",
+            self.apriltag_callback,
+            qos.qos_profile_sensor_data,
+        )
+
+        self.detections: list[tuple[int, np.ndarray]] = []
+
+    def apriltag_callback(self, msg: AprilTagDetectionArray):
+        self.detections = []
+
+        for d in msg.detections:
+            center = np.array([d.centre.x, d.centre.y]) / IMAGE_DIMS
+            self.detections.append((d.id, center))
 
     def move_timer_callback(self):
-        pass
+
+        msg = Twist()
+
+        match self.move_state:
+            case MoveState.FORWARD:
+                print("forward")
+                msg.linear.x = 0.1
+                msg.angular.z = self.turn
+
+        print(msg)
+
+        if DO_MOVE:
+            self.move_publisher.publish(msg)
+
+    def odom_callback(self, odom: Odometry):
+        raw_position = to_np(odom.pose.pose.position)
+        raw_orientation = euler_from_quaternion(odom.pose.pose.orientation)
+
+        if self.base_position is None:
+            self.base_position = raw_position
+
+        if self.base_orientation is None:
+            self.base_orientation = raw_orientation
+
+        self.position = raw_position - self.base_position
+        self.orientation = raw_orientation - self.base_orientation
+
+        print(f"Position {self.position}")
+
 
     def image_callback(self, msg: Image):
         self.get_logger().debug("got frame")
 
-        img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        # PERCEPTION
 
+        img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
         yellowRange = ((25, 40, 100), (80, 255, 255))
         yellow_balls = self.detect_balls(img, yellowRange, 20)
-        blue_ball = self.find_blue_ball(img)
+        # blue_ball = self.find_blue_ball(img)
 
         # draw a dot on each ball, decreasing in color intensity
         for i, ball in enumerate(yellow_balls):
             cv2.circle(img, (int(ball[0]), int(ball[1])), 10, (0, 255, 0), -1)
 
-        if blue_ball is not None:
-            x, y = blue_ball.centroid
-            cv2.circle(img, (int(x), int(y)), 10, (255, 0, 0), -1)
+        # if blue_ball is not None:
+        #     x, y = blue_ball.centroid
+        #     cv2.circle(img, (int(x), int(y)), 10, (255, 0, 0), -1)
 
         self.show_img(img, "Camera")
 
         cv2.waitKey(1)
+
+
+        # CONTROL/PLANNING
+
+        if len(yellow_balls) > 0:
+            yellow_balls = yellow_balls / IMAGE_DIMS # normalize
+
+            # seek largest ball
+            target = yellow_balls[0]
+            x_error = 0.5 - target[0]
+
+            self.turn = prop(x_error, 1.0, 0.1, min_error=0.03)
+            print(self.detections)
+        else:
+            self.turn = 0.0
 
     def detect_balls(self, img, colorRange, area_min, name="mask") -> np.ndarray:
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -129,7 +230,7 @@ class Soccer(Node):
         centroids = centroids[idx]
 
         # Sort by area
-        idx = np.argsort(stats[:, cv2.CC_STAT_AREA])
+        idx = np.argsort(stats[:, cv2.CC_STAT_AREA])[::-1]
         stats = stats[idx]
         centroids = centroids[idx]
 
@@ -185,7 +286,7 @@ class Soccer(Node):
         preds = self.detect_objects(img)
 
         for p in preds:
-            if "ball" in p.label:
+            if p.label == "sports ball":
                 x1, y1, x2, y2 = p.bbox
                 bbox_area = img[y1:y2, x1:x2]
                 avg_color = np.mean(bbox_area, axis=(0, 1))[::-1]
